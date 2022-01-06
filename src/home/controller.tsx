b03317e08cd5d9b90414a69ffbf2967f5038b495
Evert Pot <me@evertpot.com>
2022-01-06 01:26:44 -0500

    Episode 6 results!

--- src/home/controller.tsx
+++ src/home/controller.tsx
@@ -1,14 +1,14 @@
 import Controller from '@curveball/controller';
 import { Context } from '@curveball/core';
 
+import html from './formats/html';
+
 class HomeController extends Controller {
 
   get(ctx: Context) {
 
-    ctx.response.type = 'application/json';
-    ctx.response.body = {
-      title: 'Hello World!'
-    };
+    ctx.response.type = 'text/html; charset=utf-8';
+    ctx.response.body = html();
 
   }
 
---
+++ src/home/formats/html.tsx
@@ -0,0 +1,43 @@
+export default function html() {
+
+  return <html>
+    <head>
+      <title>Time Tracking App</title>
+      <link rel="stylesheet" href="/assets/css/main.css" type="text/css" />
+    </head>
+    <body>
+      <header>
+        <h1>Time Tracking App</h1>
+        
+        <nav>
+          <ul>
+            <li><a href="#">My timesheet</a></li>
+            <li><a href="#">Projects</a></li>
+            <li><a href="#">Clients</a></li>
+            <li><a href="#">People</a></li>
+          </ul>
+        </nav>
+      </header>
+      <main>
+      <h1>My timesheet</h1>
+      <div>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse tincidunt augue vel blandit scelerisque. Praesent vel mi eu enim fringilla maximus. Suspendisse non dignissim ligula. Ut feugiat eu dolor eget fermentum. Sed quis enim tempus, cursus sapien et, molestie ex. Aliquam fermentum sagittis dapibus. Phasellus mattis maximus neque, et condimentum massa porttitor convallis. Curabitur a cursus magna. Nam at lacinia enim. Pellentesque nec vestibulum lectus, quis consectetur ligula. Duis enim sem, consectetur at blandit non, eleifend eu massa. Donec sed malesuada leo. Fusce et turpis congue, ultrices odio et, dictum nisi.
+        </p>
+        <p>
+          Vestibulum et lectus leo. Donec sem mi, aliquet sed leo faucibus, venenatis volutpat felis. Integer congue, libero nec tincidunt laoreet, turpis ex tempor urna, aliquam porta tortor risus at ex. Mauris lorem metus, elementum et gravida vel, vestibulum sed dolor. Nulla aliquet commodo mi non gravida. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque elementum lobortis sem, in bibendum velit. Nulla erat libero, dignissim vel ultrices euismod, molestie sed neque. Nam porttitor orci quis arcu pulvinar fermentum. Donec vitae dignissim sapien. Maecenas tristique nulla ut lectus commodo bibendum. Duis scelerisque sed augue sit amet sodales. Quisque fringilla pulvinar sem. Pellentesque aliquam consectetur justo eu eleifend.
+        </p>
+        <p>
+          Vivamus hendrerit rutrum lacus, ut ornare turpis fringilla egestas. Nam id elit nec leo dapibus porta et mattis ex. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam accumsan in ante eget tempus. Donec quis magna vel diam ullamcorper tempus. Nullam efficitur viverra magna. Integer et dolor sit amet mi euismod auctor nec vitae tellus. Integer at mollis orci, at pretium nunc. Nulla facilisi. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;
+        </p>
+        <p>
+          Ut id scelerisque purus. Pellentesque feugiat rutrum leo, sit amet tempus nulla placerat facilisis. Morbi malesuada dolor sed erat tempus, vitae vestibulum elit egestas. Duis ac nunc at diam rutrum rutrum. Sed lobortis in odio et interdum. Vivamus ut lectus at nunc dignissim pharetra. Aliquam pharetra magna id tempor iaculis. Cras ut laoreet justo. In eleifend mauris sed felis interdum facilisis. Nulla eget fringilla ex. Cras in diam orci. Vestibulum dignissim ante nunc, eu tincidunt diam scelerisque sed. Integer venenatis ipsum non metus venenatis, semper consectetur augue imperdiet. Vestibulum nec malesuada diam.
+        </p>
+        <p>
+          Mauris maximus arcu eget enim gravida consectetur a vel sapien. Integer blandit lacinia mi et hendrerit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Vestibulum quis purus nec neque facilisis imperdiet vitae vitae leo. Nulla mollis imperdiet diam non condimentum. Donec eget bibendum turpis, mattis porttitor lorem. Praesent arcu risus, rhoncus sagittis ex sit amet, pretium pellentesque ligula. Etiam dapibus maximus mauris nec interdum. Phasellus pharetra, dui id molestie euismod, ante diam dignissim justo, eget sollicitudin erat arcu eu odio. Sed volutpat molestie gravida.
+       </p></div>
+      </main> 
+    </body>
+  </html>;
+
+}
