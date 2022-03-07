e92a98bc9be91d3a48dc47cd03ab4d06ddcfd64f
Evert Pot <me@evertpot.com>
2022-03-06 21:12:18 -0500

    Episode 11 changes
    Accidentally also shipped some other code with this that was not
    committed before this episode.
    
    Stream for this episode: https://youtu.be/rmfAkBH37ng

---
+++ src/components/person/PeoplePage.tsx
@@ -0,0 +1,30 @@
+import * as React from 'react';
+import { useLocation } from 'react-router-dom';
+import { useResource } from 'react-ketting';
+
+import { CollectionLinks } from '../CollectionLinks';
+import { getTitle } from '../../resource-util';
+
+import { Link } from 'react-router-dom';
+
+export function PeoplePage() {
+
+  const location = useLocation();
+
+  const { loading, error, resourceState, resource } = useResource(location.pathname);
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div className="error">{error.message}</div>;
+
+  return <>
+    <div className="page-header"><h1>{getTitle(resourceState)}</h1></div>
+    <div>
+      <CollectionLinks resource={resource} />
+    </div>
+    <div>
+      <Link className="btn btn-primary" to='/person/new'>Add new user</Link>
+    </div>
+  </>;
+
+}
+
